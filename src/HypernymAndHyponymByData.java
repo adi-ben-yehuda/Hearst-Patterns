94e8b6d5dcb4eacea5e3f0c84df4448ed8cdb08c
adi-ben-yehuda <75027826+adi-ben-yehuda@users.noreply.github.com>
2023-03-15 17:42:07 +0200

    Update HypernymAndHyponymByData.java

--- src/HypernymAndHyponymByData.java
+++ src/HypernymAndHyponymByData.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import java.util.Comparator;
 
 /**
- * @author Adi Ben Yehuda 211769757
+ * @author Adi Ben Yehuda
  * @since 2022-06-12
  */
 public class HypernymAndHyponymByData {
